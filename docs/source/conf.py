408099bd6374fe793ac1f3dc9a5223142e7d016e
Markus Bursch <49681429+maburs@users.noreply.github.com>
2023-08-01 14:58:50 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinx_panels', #mbursch
+#    'sphinx_panels', #mbursch
 ]
 
 # Add any paths that contain custom static files (such as style sheets) here,
