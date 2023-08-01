dd4069c72f838e84034b0426645ed49e1a837320
Markus Bursch <49681429+maburs@users.noreply.github.com>
2023-08-01 13:59:21 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,6 @@ html_theme = 'sphinx_rtd_theme'
 epub_show_urls = 'footnote'
 
 # -- Theme Options mbursch
-html_logo = "../figures/M_circle.png"
 html_theme_options = {
     'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
     'analytics_anonymize_ip': False,
@@ -44,7 +43,7 @@ html_theme_options = {
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'vcs_pageview_mode': '',
-    'style_nav_header_background': '#004e9f', #was white
+    'style_nav_header_background': 'white',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
