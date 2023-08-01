3c3f53a363760272362b1891bab0f645c1852aa7
Markus Bursch <49681429+maburs@users.noreply.github.com>
2023-08-01 15:12:12 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ extensions = [
     'myst_parser',
     'sphinx_design',
 ]
-
+myst_enable_extensions = ["colon_fence"]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
